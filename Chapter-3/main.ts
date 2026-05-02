22f89a881eb4a25ebeaedc957bc3d1b61e93cc5f
eb_66 <ebjehovah@gmail.com>
2026-05-02 06:30:25 +0300

    changes made to recursion.ts

--- Chapter-3/main.ts
+++ Chapter-3/main.ts
@@ -1,6 +1,13 @@
 import {countDown, theCallStack, factorial} from './recursion';
 
-// console.log(countDown(10));
-// theCallStack('Ari');
+console.log('countDown: ');
+console.log(countDown(10));
+console.log(); //visual separator for console clarity
 
+console.log('Callstack: ');
+theCallStack('Ari');
+console.log(); //visual separator for console clarity
+
+console.log('factorial: ');
 console.log(factorial(5));
+
