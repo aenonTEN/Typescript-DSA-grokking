4ae858b840b5dab6c5aa27ae028e850b74268161
eb_66 <ebjehovah@gmail.com>
2026-05-05 04:15:14 +0300

    changes made

--- Chapter-3/main.ts
+++ Chapter-3/main.ts
@@ -2,11 +2,11 @@ import {countDown, theCallStack, factorial} from './recursion';
 
 console.log('countDown: ');
 console.log(countDown(10));
-console.log(); //visual separator for console clarity
+console.log(); //-----visual separator for console clarity
 
 console.log('Callstack: ');
 theCallStack('Ari');
-console.log(); //--visual separator for console clarity
+console.log(); //------visual separator for console clarity
 
 console.log('factorial: ');
 console.log(factorial(5));
